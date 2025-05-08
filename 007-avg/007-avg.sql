b6a09829f2d4248ff8e24e6a4ae1d3bbd4b0d10c
JC <jc.foroxinnovacion@gmail.com>
2025-05-08 13:54:55 +0200

    007-avg

---
+++ 007-avg/007-avg.sql
@@ -0,0 +1,21 @@
+/* 
+AVG()
+Muestra el promedio de una columna con valores (TODOS)
+o siguiendo criterios de busqueda, ...
+*/
+
+# Consulta con el número de alquileres, importe total, 
+# promedio (con AVG()) y promedio calculado manualmente.
+
+SELECT 
+COUNT(amount) AS numero_alquileres,
+SUM(amount) AS importe_total_alquileres,
+AVG(amount) AS promedio_avg,
+SUM(amount) / COUNT(amount) AS promedio_manual
+FROM payment;
+
+# +-------------------+--------------------------+--------------+-----------------+
+# | numero_alquileres | importe_total_alquileres | promedio_avg | promedio_manual |
+# +-------------------+--------------------------+--------------+-----------------+
+# |             16044 |                 67406.56 |     4.201356 |        4.201356 |
+# +-------------------+--------------------------+--------------+-----------------+
