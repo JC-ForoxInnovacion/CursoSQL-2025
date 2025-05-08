185afa9273ef77a401933b279b996adfa7312866
JC <jc.foroxinnovacion@gmail.com>
2025-05-08 09:18:50 +0200

    Add 003-order-by

---
+++ 003-order-by/003-order-by.sql
@@ -0,0 +1,27 @@
+/**
+ORDER BY
+  Ordena los registros de forma ascendente (ASC)
+  o descente (DESC)
+
+*/
+
+# Muestra todos los registros ordenados ascendentemente
+# por la columna 'first_name' 
+SELECT *
+FROM actor 
+ORDER BY first_name ASC;
+
+# Muestra todos los registros cuyo 'first_name' = 'PENELOPE'
+# y los ordena de forma ascendente por 'last_name' 
+SELECT *
+FROM actor 
+WHERE first_name = 'PENELOPE'
+ORDER BY last_name ASC;
+
+
+# Muestra todos los registros cuyo 'first_name' = 'PENELOPE'
+# y los ordena de forma descendente por 'actor_id' 
+SELECT *
+FROM actor 
+WHERE first_name = 'PENELOPE'
+ORDER BY actor_id DESC;
